d6877c19094583b01ed866bbf676b3927863b122
PsychoAct <vadimgalk@mail.ru>
2022-11-11 22:14:35 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApp15/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApp15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Вывод = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // Вывод
+            // 
+            this.Вывод.AutoSize = true;
+            this.Вывод.Location = new System.Drawing.Point(149, 114);
+            this.Вывод.Name = "Вывод";
+            this.Вывод.Size = new System.Drawing.Size(0, 13);
+            this.Вывод.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.panel1.Location = new System.Drawing.Point(258, 250);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(204, 120);
+            this.panel1.TabIndex = 1;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Вывод);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Вывод;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WindowsFormsApp15/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp15
+{
+    public partial class Form1 : Form
+    {
+        public int total = 0;
+        public int left_click = 0;
+        public int right_click = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs myArgs = (MouseEventArgs)e;
+            total++;
+            if (myArgs.Button == MouseButtons.Left)
+                left_click++;
+            if (myArgs.Button == MouseButtons.Right)
+                right_click++;
+            Вывод.Text = ("Общее количество : " + total.ToString()+ "Количество нажатий левой кнопки: " +left_click.ToString()+ "Количество нажатий правой кнопки: "+ right_click.ToString());
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
